a3cf038963c8a850c9b0efb1475c5eee93f8a801
Samuel Shaw <samuelshaw2000@gmail.com>
2022-06-03 13:29:06 -0700

    ActuatorParser.java Completed

--- src/cs350s22/component/ui/parser/ActuatorParser.java
+++ src/cs350s22/component/ui/parser/ActuatorParser.java
@@ -1,9 +1,99 @@
 package cs350s22.component.ui.parser;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cs350s22.component.sensor.A_Sensor;
+import cs350s22.support.Identifier;
+import cs350s22.test.ActuatorPrototype;
+
 public class ActuatorParser implements SubParser {
-    @Override
-    public void parse(String[] commandTextSplit, A_ParserHelper parserHelper) {
-        //Provided arguments start at commandTextSplit[2]
-        int index = 2;
-    }
+	@Override
+	public void parse(String[] args, A_ParserHelper parserHelper) {
+		// Provided arguments start at commandTextSplit[2], type of actuator guaranteed
+		// to be first argument, id the second
+		// initialize all variables
+		List<Identifier> groups = new ArrayList<Identifier>();
+		List<A_Sensor> sensors = new ArrayList<A_Sensor>();
+		double accelerationLeadin = 0;
+		double accelerationLeadout = 0;
+		double accelerationRelax = 0;
+		double velocityLimit = 0;
+		double valueInitial = 0;
+		double valueMin = 0;
+		double valueMax = 0;
+		double inflectionJerkThreshold = 0;
+		String actuatorType = args[2].toUpperCase();
+		if (actuatorType != "LINEAR" || actuatorType != "ROTARY")
+			System.out.println("INVALID ACTUATOR TYPE");
+		Identifier id = Identifier.make(args[3]);
+		List<String> possibleCommands = List.of("ACCELERATION", "JERK", "VELOCITY");
+		int index = 4;
+		while (index < args.length) {
+			switch (args[index].toUpperCase()) {
+			case "SENSOR":
+			case "SENSORS":
+				index++;
+				// not yet implemented
+				while (!possibleCommands.contains(args[index].toUpperCase())) {
+					index++;
+					sensors.add(parserHelper.getSymbolTableSensor().get(Identifier.make(args[index])));
+				}
+				break;
+			case "ACCELERATION":
+				index++;
+				while (!possibleCommands.contains(args[index].toUpperCase())) {
+					if (args[index].toUpperCase() == "LEADIN") {
+						index++;
+						accelerationLeadin = Double.parseDouble(args[index]);
+						index++;
+					} else if (args[index].toUpperCase() == "LEADOUT") {
+						index++;
+						accelerationLeadout = Double.parseDouble(args[index]);
+						index++;
+					} else if (args[index].toUpperCase() == "RELAX") {
+						index++;
+						accelerationRelax = Double.parseDouble(args[index]);
+						index++;
+					}
+				}
+				break;
+			case "VELOCITY":
+				index++;
+				while (!possibleCommands.contains(args[index].toUpperCase())) {
+					if (args[index].toUpperCase() == "LIMIT") {
+						index++;
+						velocityLimit = Double.parseDouble(args[index]);
+					} else if (args[index].toUpperCase() == "MIN") {
+						index++;
+						valueMin = Double.parseDouble(args[index]);
+					} else if (args[index].toUpperCase() == "MAX") {
+						index++;
+						valueMax = Double.parseDouble(args[index]);
+					} else if (args[index].toUpperCase() == "INITIAL") {
+						index++;
+						valueInitial = Double.parseDouble(args[index]);
+					} else if (args[index].toUpperCase() == "VALUE")
+						index++;
+				}
+				break;
+			case "JERK":
+				index++;
+				if (args[index].toUpperCase() == "LIMIT") {
+					index++;
+					inflectionJerkThreshold = Double.parseDouble(args[index]);
+				} else
+					index++;
+				break;
+			default:
+				groups.add(Identifier.make(args[index]));
+				index++;
+				break;
+			}
+		}
+		ActuatorPrototype actuator = new ActuatorPrototype(id, groups, accelerationLeadin, accelerationLeadout,
+				accelerationRelax, velocityLimit, valueInitial, valueMin, valueMax, inflectionJerkThreshold, sensors);
+
+		parserHelper.getSymbolTableActuator().add(id, actuator);
+	}
 }
