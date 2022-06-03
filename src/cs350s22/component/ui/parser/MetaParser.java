0399f72c4e778233e043021eda07dba689da2622
Hunter T <strangeranger01@randomserver.xyz>
2022-06-03 12:57:46 -0700

    Add v2 of pdf, docs, and jar

--- src/cs350s22/component/ui/parser/MetaParser.java
+++ src/cs350s22/component/ui/parser/MetaParser.java
@@ -1,9 +1,84 @@
 package cs350s22.component.ui.parser;
 
+import java.io.IOException;
+
 /**
  *
  */
 public class MetaParser implements SubParser {
+    String[] commandTextSplit;
+    A_ParserHelper parserHelper;
+
     @Override
-    public void parse(String[] commandTextSplit, A_ParserHelper parserHelper) { }
+    public void parse(String[] commandTextSplit, A_ParserHelper parserHelper) {
+        this.commandTextSplit = commandTextSplit;
+        this.parserHelper     = parserHelper;
+
+        switch (commandTextSplit[0].toUpperCase()) {
+            case "@CLOCK":
+                setClockHelper();
+                break;
+            case "@EXIT":
+                // TODO: Currently produces a not initialized error. Fix?
+                parserHelper.exit();
+                System.exit(0);
+                break;
+            case "@RUN":
+                try {
+                    parserHelper.run(commandTextSplit[1]);
+                } catch (ParseException | IOException e) {
+                    throw new RuntimeException(e);
+                }
+                break;
+            case "@CONFIGURE":
+                configure();
+                break;
+            default:
+                System.out.println("Invalid META command: " + commandTextSplit[0]);
+                break;
+        }
+    }
+
+    /**
+     *
+     */
+    private void setClockHelper() {
+        if (commandTextSplit.length < 2) {
+            System.out.println("Missing @CLOCK argument");
+            return;
+        }
+
+        switch (commandTextSplit[0].toUpperCase()) {
+
+        }
+    }
+
+    /**
+     * 0 means pause, 1 means resume
+     *
+     * @param action Whether to pause or resume the ______.
+     */
+    private void setClockState(int action) { }
+
+    /**
+     *
+     */
+    private void setClockOnestep() { }
+
+    /**
+     *
+     * @param onestep
+     */
+    private void setClockOnestep(int onestep) { }
+
+    /**
+     *
+     * @param rate
+     */
+    private void setClockRate(int rate) { }
+
+    /**
+     *
+     */
+    private void configure() { }
 }
