bfad82935aef6edcf2c3aa89b3fd67bea449605f
Samuel Shaw <samuelshaw2000@gmail.com>
2022-06-07 13:13:35 -0700

    BuildParser Completed

--- src/cs350s22/component/ui/parser/BuildParser.java
+++ src/cs350s22/component/ui/parser/BuildParser.java
@@ -1,9 +1,35 @@
 package cs350s22.component.ui.parser;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cs350s22.component.A_Component;
+import cs350s22.support.Identifier;
+
 /**
  *
  */
 public class BuildParser implements SubParser {
-    @Override
-    public void parse(String[] commandTextSplit, A_ParserHelper parserHelper) { }
+	@Override
+	public void parse(String[] args, A_ParserHelper parserHelper) {
+		int index = 1;
+		List<A_Component> components = new ArrayList<A_Component>();
+		if (args[index].equalsIgnoreCase("NETWORK")) {
+			index = 4;
+			if (index < args.length) {
+				components.add(parserHelper.getSymbolTableController().get(Identifier.make(args[index])));
+				index++;
+				if (index < args.length) {
+					components.add(parserHelper.getSymbolTableActuator().get(Identifier.make(args[index])));
+					index++;
+					if (index < args.length) {
+						components.add(parserHelper.getSymbolTableSensor().get(Identifier.make(args[index])));
+						index++;
+					}
+				}
+			}
+		}
+		parserHelper.getControllerMaster().addComponents(components);
+		parserHelper.getNetwork().writeOutput();
+	}
 }
