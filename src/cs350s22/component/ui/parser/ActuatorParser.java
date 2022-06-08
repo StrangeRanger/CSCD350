96405c264e5b2579f589bd28a6ed185581e80dab
Samuel Shaw <samuelshaw2000@gmail.com>
2022-06-08 13:37:23 -0700

    Removed infinite loop

--- src/cs350s22/component/ui/parser/ActuatorParser.java
+++ src/cs350s22/component/ui/parser/ActuatorParser.java
@@ -1,10 +1,11 @@
 package cs350s22.component.ui.parser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cs350s22.component.sensor.A_Sensor;
 import cs350s22.support.Identifier;
 import cs350s22.test.ActuatorPrototype;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * The actuator command is responsible for creating an actuator and optionally
@@ -13,105 +14,105 @@ import java.util.List;
  * ...
  */
 public class ActuatorParser implements SubParser {
-	private final String[]       args;
-    private final A_ParserHelper parserHelper;
+	private final String[] args;
+	private final A_ParserHelper parserHelper;
 
-    /** Constructor. */
-    public ActuatorParser(String[] args, A_ParserHelper parserHelper) {
-        this.args         = args;
-        this.parserHelper = parserHelper;
-    }
+	/** Constructor. */
+	public ActuatorParser(String[] args, A_ParserHelper parserHelper) {
+		this.args = args;
+		this.parserHelper = parserHelper;
+	}
 
-    @Override
-    public void parse() {
-        // Provided arguments start at commandTextSplit[2], type of actuator guaranteed
-        // to be first argument, id the second
-        // initialize all variables
-        List<Identifier> groups                  = new ArrayList<>();
-        List<A_Sensor>   sensors                 = new ArrayList<>();
-        double           accelerationLeadin      = 0;
-        double           accelerationLeadout     = 0;
-        double           accelerationRelax       = 0;
-        double           velocityLimit           = 0;
-        double           valueInitial            = 0;
-        double           valueMin                = 0;
-        double           valueMax                = 0;
-        double           inflectionJerkThreshold = 0;
-        int              index                   = 4;
-        Identifier       id                      = Identifier.make(args[3]);
-        List<String>     possibleCommands = List.of("ACCELERATION", "JERK", "VELOCITY");
+	@Override
+	public void parse() {
+		// Provided arguments start at commandTextSplit[2], type of actuator guaranteed
+		// to be first argument, id the second
+		// initialize all variables
+		List<Identifier> groups = new ArrayList<>();
+		List<A_Sensor> sensors = new ArrayList<>();
+		double accelerationLeadin = 0;
+		double accelerationLeadout = 0;
+		double accelerationRelax = 0;
+		double velocityLimit = 0;
+		double valueInitial = 0;
+		double valueMin = 0;
+		double valueMax = 0;
+		double inflectionJerkThreshold = 0;
+		int index = 4;
+		Identifier id = Identifier.make(args[3]);
+		List<String> possibleCommands = List.of("ACCELERATION", "JERK", "VELOCITY");
 
-        try {
-            while (index < args.length) {
-                switch (args[index].toUpperCase()) {
-					case "SENSOR":
-					case "SENSORS":
-                        index++;
-                        // not yet implemented
-                        while (! possibleCommands.contains(args[index].toUpperCase())) {
-                            index++;
-                            sensors.add(parserHelper.getSymbolTableSensor().get(Identifier.make(args[index])));
-                        }
-                        break;
-                    case "ACCELERATION":
-                        index++;
-                        while (! possibleCommands.contains(args[index].toUpperCase())) {
-                            if (args[index].equalsIgnoreCase("LEADIN")) {
-                                index++;
-                                accelerationLeadin = Double.parseDouble(args[index]);
-                                index++;
-                            } else if (args[index].equalsIgnoreCase("LEADOUT")) {
-                                index++;
-                                accelerationLeadout = Double.parseDouble(args[index]);
-                                index++;
-                            } else if (args[index].equalsIgnoreCase("RELAX")) {
-                                index++;
-                                accelerationRelax = Double.parseDouble(args[index]);
-                                index++;
-                            }
-                        }
-                        break;
-                    case "VELOCITY":
-                        index++;
-                        while (! possibleCommands.contains(args[index].toUpperCase())) {
-                            if (args[index].equalsIgnoreCase("LIMIT")) {
-                                index++;
-                                velocityLimit = Double.parseDouble(args[index]);
-                            } else if (args[index].equalsIgnoreCase("MIN")) {
-                                index++;
-                                valueMin = Double.parseDouble(args[index]);
-                            } else if (args[index].equalsIgnoreCase("MAX")) {
-                                index++;
-                                valueMax = Double.parseDouble(args[index]);
-                            } else if (args[index].equalsIgnoreCase("INITIAL")) {
-                                index++;
-                                valueInitial = Double.parseDouble(args[index]);
-                            } else if (args[index].equalsIgnoreCase("VALUE"))
-                                index++;
-                        }
-                        break;
-                    case "JERK":
-                        index++;
-                        if (args[index].equalsIgnoreCase("LIMIT")) {
-                            index++;
-                            inflectionJerkThreshold = Double.parseDouble(args[index]);
-                        } else
-                            index++;
-                        break;
-                    default:
-                        groups.add(Identifier.make(args[index]));
-                        index++;
-                        break;
-                }
-            }
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Logic Error in AcuatorParser, actuator has most likely been created fine");
-        }
+		try {
+			while (index < args.length) {
+				switch (args[index].toUpperCase()) {
+				case "SENSOR":
+				case "SENSORS":
+					index++;
+					// not yet implemented
+					while (!possibleCommands.contains(args[index].toUpperCase())) {
+						index++;
+						sensors.add(parserHelper.getSymbolTableSensor().get(Identifier.make(args[index])));
+					}
+					break;
+				case "ACCELERATION":
+					index++;
+					while (!possibleCommands.contains(args[index].toUpperCase())) {
+						if (args[index].equalsIgnoreCase("LEADIN")) {
+							index++;
+							accelerationLeadin = Double.parseDouble(args[index]);
+							index++;
+						} else if (args[index].equalsIgnoreCase("LEADOUT")) {
+							index++;
+							accelerationLeadout = Double.parseDouble(args[index]);
+							index++;
+						} else if (args[index].equalsIgnoreCase("RELAX")) {
+							index++;
+							accelerationRelax = Double.parseDouble(args[index]);
+							index++;
+						}
+					}
+					break;
+				case "VELOCITY":
+					index++;
+					while (!possibleCommands.contains(args[index].toUpperCase())) {
+						if (args[index].equalsIgnoreCase("LIMIT")) {
+							index++;
+							velocityLimit = Double.parseDouble(args[index]);
+						} else if (args[index].equalsIgnoreCase("MIN")) {
+							index++;
+							valueMin = Double.parseDouble(args[index]);
+						} else if (args[index].equalsIgnoreCase("MAX")) {
+							index++;
+							valueMax = Double.parseDouble(args[index]);
+						} else if (args[index].equalsIgnoreCase("INITIAL")) {
+							index++;
+							valueInitial = Double.parseDouble(args[index]);
+						} else if (args[index].equalsIgnoreCase("VALUE"))
+							index++;
+						else
+							index++;
+					}
+					break;
+				case "JERK":
+					index++;
+					if (args[index].equalsIgnoreCase("LIMIT")) {
+						index++;
+						inflectionJerkThreshold = Double.parseDouble(args[index]);
+					} else
+						index++;
+					break;
+				default:
+					groups.add(Identifier.make(args[index]));
+					index++;
+					break;
+				}
+			}
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println("Logic Error in AcuatorParser, actuator has most likely been created fine");
+		}
 
-        ActuatorPrototype actuator = new ActuatorPrototype(id, groups, accelerationLeadin,
-													       accelerationLeadout, accelerationRelax,
-													       velocityLimit, valueInitial, valueMin, valueMax,
-													       inflectionJerkThreshold, sensors);
-        parserHelper.getSymbolTableActuator().add(id, actuator);
-    }
+		ActuatorPrototype actuator = new ActuatorPrototype(id, groups, accelerationLeadin, accelerationLeadout,
+				accelerationRelax, velocityLimit, valueInitial, valueMin, valueMax, inflectionJerkThreshold, sensors);
+		parserHelper.getSymbolTableActuator().add(id, actuator);
+	}
 }
--- src/cs350s22/startup/Startup.java
+++ src/cs350s22/startup/Startup.java
@@ -6,30 +6,33 @@ import cs350s22.component.ui.parser.ParserHelper;
 
 /** Main class. */
 public class Startup {
-    private final A_ParserHelper _parserHelper = new ParserHelper();
+	private final A_ParserHelper _parserHelper = new ParserHelper();
 
-    /** Main method. */
-    public static void main(final String[] arguments) throws Exception {
-        Startup startup = new Startup();
+	/** Main method. */
+	public static void main(final String[] arguments) throws Exception {
+		Startup startup = new Startup();
 
-        // This command must come first. The filenames do not matter here.
-        startup.parse(
-                "@CONFIGURE LOG \"a.txt\" DOT SEQUENCE \"b.txt\" NETWORK \"c.txt\" XML \"d.txt\"");
+		// This command must come first. The filenames do not matter here.
 
-        // Run your tests like this.
-        startup.parse("@exit");
-    }
+		// Run your tests like this.
+		startup.parse("@CONFIGURE LOG \"a.txt\" DOT SEQUENCE \"b.txt\" NETWORK \"c.txt\" XML \"d.txt\"");
 
-    /**
-     *
-     * @param parse
-     * @throws Exception
-     */
-    private void parse(final String parse) throws Exception {
-        System.out.println("PARSE> " + parse);
+		startup.parse(
+				"CREATE ACTUATOR LINEAR a1 ACCELERATION LEADIN 0.1 LEADOUT -0.2 RELAX 0.3 VELOCITY LIMIT 5 VALUE MIN 1 MAX 20 INITIAL 2 JERK LIMIT 3");
+		// startup.parse("SEND MESSAGE PING");
+		startup.parse("@exit");
+	}
 
-        Parser parser = new Parser(_parserHelper, parse);
+	/**
+	 *
+	 * @param parse
+	 * @throws Exception
+	 */
+	private void parse(final String parse) throws Exception {
+		System.out.println("PARSE> " + parse);
 
-        parser.parse();
-    }
+		Parser parser = new Parser(_parserHelper, parse);
+
+		parser.parse();
+	}
 }
